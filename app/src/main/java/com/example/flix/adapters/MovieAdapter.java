12ab0eefa34bf6501c548cd08091ca33ef10df5d
thinhhoangpham <hoangthinh.pham@gmail.com>
2022-03-24 22:10:30 -0500

    Unit 2

--- app/src/main/java/com/example/flix/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flix/adapters/MovieAdapter.java
@@ -1,18 +1,24 @@
 package com.example.flix.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.flix.DetailActivity;
 import com.example.flix.R;
 import com.example.flix.models.Movie;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -49,6 +55,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
+        RelativeLayout container;
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
@@ -58,12 +65,26 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
         }
 
+        // 1. register click listener on whole row
         public void bind(Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
             Glide.with(context).load(movie.getPosterPath()).into(ivPoster);
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    // 2. navigate to new activity
+                    //Toast.makeText(context, movie.getTitle(), Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(context, DetailActivity.class);
+                    //i.putExtra("title", movie.getTitle());
+                    i.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(i);
+                }
+            });
         }
     }
 }
--- app/src/main/java/com/example/flix/models/Movie.java
+++ app/src/main/java/com/example/flix/models/Movie.java
@@ -3,19 +3,31 @@ package com.example.flix.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
+
+@Parcel
 
 public class Movie {
     String posterPath;
     String title;
     String overview;
+    float rating;
+    int movieId;
 
     public Movie(JSONObject jsonobject) throws JSONException {
         posterPath = jsonobject.getString("poster_path");
         title = jsonobject.getString("title");
         overview = jsonobject.getString("overview");
+        rating = (float) jsonobject.getDouble("vote_average");
+        movieId = jsonobject.getInt("id");
+    }
+
+    //Empty constructor needed by parcel
+    public Movie() {
+
     }
 
     public static List<Movie> fromJSONArray(JSONArray movieJSONArray) throws JSONException {
@@ -37,4 +49,8 @@ public class Movie {
     public String getOverview() {
         return overview;
     }
+
+    public float getRating() {return  rating;}
+
+    public int getMovieId() {return movieId;}
 }
